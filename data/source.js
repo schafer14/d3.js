c358557c22733d43cdd4e56874ec1e2a0b35554a
Banner <banner.schafer@gmail.com>
2014-04-04 22:39:35 +0800

    added template for d3 projects

--- data/source.js
+++ data/source.js
@@ -4,7 +4,9 @@ var height = 800;
 var canvas = d3.select("body")
 	.append("svg")
 	.attr("width", width)
-	.attr("height", height);
+	.attr("height", height)
+	.append("g")
+	.attr("transform", "translate(30, 34)");
 
 d3.json("data.json", function(error, graph) {
 	var widthScale = d3.scale.linear()
@@ -15,18 +17,18 @@ d3.json("data.json", function(error, graph) {
 		.domain([50, 150])
 		.range(["red", "blue"]);
 
-	var bubble = canvas.selectAll(".bubble")
+	var bubbles = canvas.selectAll(".bubble")
 	    .data(graph.bubbles)
 	    .enter().append("circle")
       		.attr("class", "bubble")
-      		// .attr("r", function(d) { return widthScale(d.size) })
       		.attr("r", function(d) { return d.size })
       		.attr("cx", function(d, i) {return d.size * (i + 1)})
       		.attr("cy", 250)
       		.style("fill", function(d) { return colorScale(d.size); });
 
-	var title = bubble.append("title")
+	var title = bubbles.append("title")
 	    .text(function(d) { return d.name; });
+
 });
 
 
